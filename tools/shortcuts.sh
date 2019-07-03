369f8023ecb9880106833f0116cac046a9b45c09
Steven Swanson <swanson@cs.ucsd.edu>
2019-07-03 06:44:03 +0000

    file full of useful command lines

---
+++ tools/shortcuts.sh
@@ -0,0 +1,2 @@
+BRANCH_BREAKDOWN="./archlab_run --engine papi --stat PAPI_TOT_CYC --stat PAPI_BR_INS --stat PAPI_BR_PRC --stat PAPI_TOT_INS --stat UOPS_RETIRED"
+PIPELINE_BREAKDOWN="./archlab_run --engine papi --stat PAPI_TOT_CYC --stat UOPS_RETIRED --stat IDQ:ALL_MITE_UOPS --stat UOPS_EXECUTED --stat PAPI_TOT_INS"
