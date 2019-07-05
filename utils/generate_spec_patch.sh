0dd56ced2ff166880a0f2350ba41c73b137c3830
Steven Swanson <swanson@cs.ucsd.edu>
2019-07-05 00:08:39 +0000

    cleanup and reorg

--- utils/generate_spec_patch.sh
+++ utils/generate_spec_patch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-diff -x MANIFEST -ru ./benchmarks/.pristine_spec2017 ./benchmarks/spec2017 > benchmarks/new-spec-CPU2017v1.0.5.patch
+diff -x MANIFEST -ru ./benchmarks/.pristine_spec2017 ./benchmarks/spec2017 > benchmarks/spec2017-setup/new-spec-CPU2017v1.0.5.patch
--- utils/install_spec.sh
+++ utils/install_spec.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+
 if ! [ -e cpu2017-1_0_5.iso ]; then
     echo "You need cpu2017-1_0_5.iso.  It's in the google drive."
     exit 1
 fi
+
+set -x
+set -v
 
 sudo mkdir /mnt/iso
 sudo mount -t iso9660 -o loop cpu2017-1_0_5.iso /mnt/iso/
@@ -11,10 +15,14 @@ cp -a /mnt/iso ./benchmarks/spec2017
 
 cd benchmarks/spec2017
 echo yes | ./install.sh
-cp -a benchmarks/spec2017 benchmarks/.pristine_spec2017
 
-patch -p3 < ../spec-CPU2017v1.0.5.patch
-ln -sf ../../cse141pp.cfg config/
+
+(cd ../../;
+ cp -a benchmarks/spec2017 benchmarks/.pristine_spec2017
+)
+
+patch -p3 < ../spec2017-setup/spec-CPU2017v1.0.5.patch
+ln -sf ../../spec2017-setup/cse141pp.cfg config/
 . shrc
 
 unset ARCHLAB_RUNNER
