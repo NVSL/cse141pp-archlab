06b20c55d8e3724659e0f4a75ea77783baba7262
Steven Swanson <swanson@cs.ucsd.edu>
2019-07-16 19:23:32 +0000

    compile with clang.  Add driver for perf measurement

---
+++ examples/x86-asm/ilp_driver.cpp
@@ -0,0 +1,24 @@
+#include "archlab.h"
+#include <stdio.h>
+
+extern "C" {
+ int not_unrolled(long int n, int a, int b);
+}
+
+int main(int argc, char *argv[]) {
+  uint64_t count;
+  archlab_add_si_option<uint64_t>("count",     count   , 100000  ,  "Iterations to run");
+  printf("%ld\n", sizeof(long int));
+  archlab_parse_cmd_line(&argc, argv);
+  pristine_machine();               // reset the machine 
+  start_timing("tag", "hello",      // Start timing.  Set an identifier 'tag' = 'hello'.  It'll appear along with the measurements in 'stats.csv'
+	       NULL);
+  uint64_t s = not_unrolled(count, 12, 20);
+  stop_timing();                    // Stop timing.
+  printf("Answer: %lu\n", s);
+  archlab_write_stats();
+  printf("Now, `cat stats.csv` will show you the results.\n");
+  printf("Try `./hello_world.exe --engine papi --stat PAPI_L2_DCA --stat PAPI_TOT_CYC --stat PAPI_TOT_INS` to gather some actual measurements.\n");
+  return 0;
+}
+
