4891f8f7138569786ba01280a72f7848188475aa
Steven Swanson <swanson@cs.ucsd.edu>
2019-07-26 22:52:10 -0700

    documentation

--- archcloud/run.py
+++ archcloud/run.py
@@ -8,6 +8,23 @@ import argparse
 import sys
 
 def main(argv):
+    """
+    This is the command line driver for Runner.py.  It should demonstrate everything you'll need to do with the library.
+
+    The assumption is that the local directory has a clone of a lab repo.  Lab repos have `lab.py` in them.  The possible fields in a `lab.py` are described in `Runner.LabSpec`
+
+    Currently, only `github.com/NVSL/CSE141pp-lab-dot-product` is setup with a correct `lab.py`, so test with that.
+
+    You can then do:
+    1. `./run.py --local` To run the lab in the local directory.
+    2. `./run.py --pristine` To run the lab in a fresh clone of the lab's repo with the local input files (specified in lab.py) copied in.
+    3. `./run.py --local --docker` to run the lab in docker container.
+    4. `./run.py --json` to dump the json version of the lab submission and response to stdout.
+    5. `./run.py --json --nop` to dump the json version of the lab submission stdout, but not run anything.
+    6. `./run.py --json --nop | ./run.py --run-json --pristine --docker` generate the submission, consume it, and run it in a pristine clone in a docker container.
+
+    The key data structures: `LabSpec`, `Submission`, `SubmissionResult` should all be fully convertible to and from dicts with `_fromdict()` and `_todict()` methods.
+    """
     parser = argparse.ArgumentParser(description='Run a lab.')
     parser.add_argument('-v', action='store_true', dest="verbose", default=False, help="Be verbose")
     parser.add_argument('--pristine', action='store_true', default=False, help="Run job locally")
@@ -18,6 +35,8 @@ def main(argv):
     parser.add_argument('--json', action='store_true', default=False, help="Dump json version of submission and response.")
     parser.add_argument('--directory', default=".", help="Directory to submit")
     parser.add_argument('--run-json', action='store_true', default=False, help="Read json submission spec from stdin")
+    parser.add_argument('--docker-image', default="cse141pp/submission-runner:0.10", help="Docker image to use")
+
 #    parser.add_argument('--repo', help="git repo")
     args = parser.parse_args(argv)
     log.basicConfig(format="{} %(levelname)-8s [%(filename)s:%(lineno)d]  %(message)s".format(platform.node()) if args.verbose else "%(message)s",
