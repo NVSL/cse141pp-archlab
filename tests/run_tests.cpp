0679945979e7f709aefb67354f802fb36c898670
Steven Swanson <swanson@cs.ucsd.edu>
2019-11-27 08:03:30 +0000

    tests for preftecher and flushing the cache

--- tests/run_tests.cpp
+++ tests/run_tests.cpp
@@ -27,24 +27,90 @@ MeasurementInterval & sleep_one() {
 	return fast;
 }
 
-TEST_F(ArchlabTest, cpu_freq_tests) {
+MeasurementInterval & big_scan() {
+	const int SIZE = 1024*1024*1024 + 1;
+	char * t = new char[SIZE];
+	volatile int s = 0;
+	for(int i = 0; i < SIZE; i+=64) {
+		s+=t[i];
+	}
+	{
+		ArchLabTimer timer;
+		timer.go();
+		for(int i = 0; i < SIZE; i+=64) {
+			s+=t[i+1]; // the +1 ensures that compiler doesn't get clever wrt to the identical loop above.
+		}
+	}
+	auto last = theDataCollector->get_intervals();
+	auto & interval = *(last->back());
+	interval.build_json();
+	return interval;
+}
 
-	theDataCollector->track_stat("ARCHLAB_WALL_TIME");
-	theDataCollector->register_stat("ARCHLAB_WALL_TIME");
+MeasurementInterval & little_scan(bool flush, int count) {
+	const int SIZE = 16*1024;
+	char * t = new char[SIZE];
+	volatile int s = 0;
+	{
+		ArchLabTimer timer;
+		timer.go();
+		
+		for(int c = 0; c < count; c++) {
+			for(int i = 0; i < SIZE; i+=1) {
+				s+=t[i];
+			}
+			if (flush)
+				theDataCollector->flush_caches();
+		}
+	}
 
-	theDataCollector->track_stat("PAPI_TOT_CYC");
-	theDataCollector->register_stat("PAPI_TOT_CYC");
+	auto last = theDataCollector->get_intervals();
+	auto & interval = *(last->back());
+	interval.build_json();
+	return interval;
+}
 
-	theDataCollector->track_stat("PAPI_REF_CYC");
-	theDataCollector->register_stat("PAPI_REF_CYC");
+TEST_F(ArchlabTest, cache_flush_test) {
+	pristine_machine();
+	theDataCollector->disable_prefetcher();
+	auto & slow = little_scan(true,10);
+	pristine_machine();
+	theDataCollector->disable_prefetcher();
+	auto & fast = little_scan(false,10);
+	
+	/*std::cerr << "slow = " << slow.get_kv().dump(4) << "\n";
+	std::cerr << "fast = " << fast.get_kv().dump(4) << "\n";
+	std::cerr << slow.get_value<float>("ARCHLAB_WALL_TIME") << "\n";
+	std::cerr << fast.get_value<float>("ARCHLAB_WALL_TIME") << "\n";*/
+	EXPECT_GT(slow.get_value<float>("ARCHLAB_WALL_TIME")/fast.get_value<float>("ARCHLAB_WALL_TIME"),5);
+}
 
+TEST_F(ArchlabTest, prefetcher_test) {
+	pristine_machine();
+	theDataCollector->disable_prefetcher();
+	auto & slow = big_scan();
+	pristine_machine();
+	theDataCollector->enable_prefetcher();
+	auto & fast = big_scan();
+	//std::cerr << "slow = " << slow.get_kv().dump(4) << "\n";
+	//std::cerr << "fast = " << fast.get_kv().dump(4) << "\n";
+
+	// This seems to show some difference in the number of
+	// prefetcher cache misses, so I guess the prefetcher setting
+	// is working.  But I don't see any impact on performance...
+	EXPECT_LT(slow.get_value<float>("PAPI_PRF_DM"), 100);
+	EXPECT_GT(fast.get_value<float>("PAPI_PRF_DM"), 10000);
+}
+
+TEST_F(ArchlabTest, cpu_freq_test) {
 	// Check whether setting cpu clock works as expected.
+
 	// cpu_frequencies[0] is the highest rate, but that turns on
 	// turboboost (I think), so use the second and third highest.
 	pristine_machine();
 	set_cpu_clock_frequency(cpu_frequencies[1]);
 	auto & fast = sleep_one();
-	//std::cerr << "fast = " << fast.get_kv().dump(4) << "\n";
+	//std::cerr << "fast = " << fast.get_kv().dump(4) << "\n";a
 	EXPECT_NEAR(fast.get_value<float>("ARCHLAB_WALL_TIME"), 1, 0.01);
 
 	pristine_machine();
@@ -64,6 +130,20 @@ TEST_F(ArchlabTest, cpu_freq_tests) {
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	archlab_parse_cmd_line(&argc, argv);
+	theDataCollector->track_stat("ARCHLAB_WALL_TIME");
+	theDataCollector->register_stat("ARCHLAB_WALL_TIME");
+
+	theDataCollector->track_stat("PAPI_TOT_CYC");
+	theDataCollector->register_stat("PAPI_TOT_CYC");
+
+	theDataCollector->track_stat("PAPI_REF_CYC");
+	theDataCollector->register_stat("PAPI_REF_CYC");
+
+	theDataCollector->track_stat("PAPI_PRF_DM");
+	theDataCollector->register_stat("PAPI_PRF_DM");
+
+	theDataCollector->track_stat("PAPI_L1_LDM");
+	theDataCollector->register_stat("PAPI_L1_LDM");
 
 	return RUN_ALL_TESTS();
 }
