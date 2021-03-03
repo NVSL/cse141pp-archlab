01c80cd08ccf74ebcfa00e164379f2174b3adf46
Steven Swanson <swanson@cs.ucsd.edu>
2021-03-03 21:37:15 +0000

    work on side channel demonstration.  I don't think it works

--- examples/side_channel.cpp
+++ examples/side_channel.cpp
@@ -15,6 +15,9 @@ namespace po = boost::program_options;
 #include <sys/ioctl.h>
 #include <cache_control/cache_control.h>
 #include<pthread.h>
+#include<sched.h>
+
+// This demo show the affects of cache interference between two cores.
 
 volatile int sink = 0;
 
@@ -25,20 +28,32 @@ uint64_t rdtsc(){
 }
 uint64_t cache_size;
 uint64_t target_lines;
+int sender_core;
+int receiver_core;
 
 char * array;
 
+volatile bool sleeping = false;
+
+
 void * thread_sender(void * arg)
 {
+	cpu_set_t where;
+	CPU_ZERO(&where);
+	CPU_SET(0, &where);
+	int r = sched_setaffinity(sender_core, sizeof(where),  &where);
+	assert(r == 0);
 	while (true) {
-		for(uint i = 0; i < 1000; i++) {
+		double start = wall_time();
+		while(wall_time() < start + 1.0) {
 			for(uint j = 0; j < target_lines; j++) {
 				array[j*cache_size]++;
 			}
 		}
-		//std::cerr << "Sleeping" << std::endl;
+		sleeping = true;
 		sleep(1);
-		//std::cerr << "Waking" << std::endl;
+		sleeping = false;
+
 	}
 	return NULL;
 }
@@ -46,9 +61,13 @@ void * thread_sender(void * arg)
 void * thread_receiver(void * arg)
 {
 	double start = wall_time();
-	//std::cerr << "Start = " << start << "\n";
+	cpu_set_t where;
+	CPU_ZERO(&where);
+	CPU_SET(3, &where);
+	int r = sched_setaffinity(receiver_core, sizeof(where),  &where);
+	assert(r == 0);
+	//std::cout << "Start = " << start << "\n";
 	while(true) {
-		
 		uint64_t before = rdtsc();
 		for(uint i = 0; i < 100; i++) {
 			for(uint j = 0; j < target_lines; j++) {
@@ -56,9 +75,11 @@ void * thread_receiver(void * arg)
 			}
 		}
 		double now = wall_time();
-
-		std::cerr << (now - start) << " " << (rdtsc() - before)/(target_lines + 100 + 0.0) << std::endl;
 	}
+
+		std::cout << (now - start) << " "
+			  << (sleeping ? 1 : 0) << " "
+			  << (rdtsc() - before)/(target_lines + 100 + 0.0) << std::endl;
 	return NULL;
 }
 
@@ -66,6 +87,8 @@ int main(int argc, char *argv[]) {
 	//uint64_t seed = 0xdeadbeafabad1dea;
 	archlab_add_si_option<uint64_t>("cache-size",  cache_size, 32*KB ,  "Cache size.");
 	archlab_add_si_option<uint64_t>("target-lines",  target_lines, 16,  "Number of cache lines to target");
+	archlab_add_option<int>("send-core",  sender_core, 0, "WHere to run the sender");
+	archlab_add_option<int>("recv-core",  receiver_core, 1, "WHere to run the recv");
 	//archlab_add_flag("enable-demo", enable_demo, false ,  "Run demos.");
 	//archlab_add_flag("no-prefetch", disable_prefetcher, false ,  "Disable the HW prefetcher.");
 		     
