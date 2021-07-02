0c3024b04ea90c41c23c854a63eb7996d40ab13a
Steven Swanson <swanson@cs.ucsd.edu>
2021-07-02 06:06:10 +0000

    post class sp21

--- archcloud/src/ArchLab/csvsort.py
+++ archcloud/src/ArchLab/csvsort.py
@@ -9,7 +9,7 @@ from .csvpretty import prettify_dicts
 def main():
     parser = argparse.ArgumentParser(description='Perform calculation on CSV files.')
     parser.add_argument('-v', action='store_true', dest="verbose", help="Be verbose")
-    parser.add_argument('--field', '-f', nargs=1, default=[0],help="fields to sort by")
+    parser.add_argument('--field', '-f', nargs='+', default=[],help="fields to sort by")
     parser.add_argument('--input', '-i',  default="-", nargs=1, help="input file")
     parser.add_argument('--out', default="-", dest="output",help="output file")
     parser.add_argument('--pretty', '-p', action="store_true", default=False, help="pretty print the result")
@@ -18,7 +18,7 @@ def main():
     log.basicConfig(level=log.DEBUG if cmdline.verbose else log.WARN)
     outfile = open(cmdline.output, "w") if cmdline.output != "-" else sys.stdout;
 
-    column = cmdline.field[0]
+
     for f in cmdline.input:
         infile = open(f) if f != "-" else sys.stdin
         inreader = csv.DictReader(infile)
@@ -27,13 +27,15 @@ def main():
         for l in inreader:
             r.append(l)
 
-        s = sorted(r, key=lambda a: a[column])
+        for field in reversed(cmdline.field):
+            r.sort(key=lambda a: a[field])
+            
         if cmdline.pretty:
-            outfile.write(prettify_dicts(s))
+            outfile.write(prettify_dicts(r))
         else:
             writer = csv.DictWriter(outfile,inreader.fieldnames)
             writer.writeheader()
-            for row in s:
+            for row in r:
                 writer.writerow(row)
 
     
