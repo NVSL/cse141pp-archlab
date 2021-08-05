c5e2e711fbc59740bb6e14d19a3f9a4f2785524e
Steven Swanson <swanson@cs.ucsd.edu>
2021-08-04 19:15:24 -0700

    fix LD_LIBRARY_PATH

--- env.sh
+++ env.sh
@@ -2,5 +2,6 @@ export ARCHLAB_ROOT=$PWD
 
 PATH=$ARCHLAB_ROOT/tools/:$ARCHLAB_ROOT/utils:$ARCHLAB_ROOT/archcloud:$PIN_ROOT/:$ARCHLAB_ROOT/installed/bin:$PATH
 
-export LD_LIBRARY_PATH=$ARCHLAB_ROOT/installed/lib
+export LD_LIBRARY_PATH=$ARCHLAB_ROOT/installed/lib:/usr/local/lib
+
 
